2fa8fd3a2b5f0b00c5bdb2636be91ae270f3aac0
Kerkeb Abdou <Kerkeb@gmail.com>
2023-05-10 14:38:34 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+// // console.log(poulet);
+// // poulet = 42;
+// // console.log(poulet);
+// age = age + 1;
+// const statut = " Chicken Lord";
+// console.log(
+//   "Hello je suis un poulet et j'ai " + age + " ans et mon statut:" + statut
+// );
+//                   0      1     2   3            4           5
+// const pouletTab = ["miam", "cat", 42, 21, "j'aime le poulet", age];
+// pouletTab[1] = "dog";
+// console.log(pouletTab.push("chicken"));
+// console.log(pouletTab);
+
+// let age = 38;
+// let age2 = 10;
+
+// function pouletAdd(num1, num2) {
+//   console.log("tolo");
+//   const resultPoulet = num1 + num2;
+//   return resultPoulet;
+// }
+// const resultDeLaFonction = pouletAdd(age, age2);
+// console.log("0002: ", resultDeLaFonction - 6);
+
+// console.log("0002: ", pouletAdd(10, 2039));
+// console.log("0002: ", pouletAdd(213, 645));
+
+// console.log(age + num);
+// const firstname = "Paul";
+// const num = "0618191254";
+// if (firstname === "Paul") {
+//   console.log("welcome " + firstname);
+// } else if (firstname === "Poulet") {
+//   console.log("welcome mon petit " + firstname);
+// } else {
+//   console.log("Go away!!");
+// }
+
+// if (num === "0618191254") {
+//   console.log("je suis superieur");
+// } else {
+//   console.log("Go away!! telephone");
+// }
+
+// console.log("je suis un grand poulet ");
+// const userName = prompt("What's your name?");
+
+// if (userName === "Paul") {
+//   const userPass = prompt("What's the secret code?");
+//   if (userPass === "secret") {
+//     console.log("Welcome, Paul!");
+//   } else {
+//     console.log("Wrong Credentials, sorry...");
+//   }
+// } else {
+//   console.log("Wrong Credentials... Sorry...");
+// }
+
+// let i = 0;
+//                   0      1      2   3        4
+const pouletTab = ["miam", "cat", 42, 21, "j'aime le poulet"];
+console.log("la taile du tableau : ", pouletTab.length);
+// while (i < pouletTab.length) {
+//   console.log(pouletTab[i]);
+//   i++;
+// }
+for (let i = 0; i < pouletTab.length; i++) {
+  console.log(pouletTab[i]);
+}
+console.log("la boulcle est fini");
